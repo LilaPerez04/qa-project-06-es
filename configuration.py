9623e714f24ace498417d064ea1768ae12ae774f
LilaPerez04 <114111230+LilaPerez04@users.noreply.github.com>
2024-01-14 21:39:28 -0600

    Versión corregida de la primera entrega

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://eab581d3-9c73-46ec-9b53-44d29b0bb28a.serverhub.tripleten-services.com"
+URL_SERVICE = "https://fa8e6126-3879-4dd8-ab50-b67b21be1cab.serverhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"  # Almacena la ruta para crear un usuario en esta variable
 KITS_PATH = "/api/v1/kits/"  # Almacena la ruta para crear un kit en esta variable
--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -13,31 +13,6 @@ def positive_assert(kit_body):
     assert kit.json()["name"] == kit_body["name"]
 
 
-def test1():
-    kit_body = data.kit_body.kit_body1
-    positive_assert(kit_body)
-
-
-def test2():
-    kit_body = data.kit_body.kit_body2
-    positive_assert(kit_body)
-
-
-def test5():
-    kit_body = data.kit_body.kit_body5
-    positive_assert(kit_body)
-
-
-def test6():
-    kit_body = data.kit_body.kit_body6
-    positive_assert(kit_body)
-
-
-def test7():
-    kit_body = data.kit_body.kit_body7
-    positive_assert(kit_body)
-
-
 def negative_assert(kit_body):
     # El cuerpo de la solicitud actualizada se guarda en la variable user_body
     kit = sender_stand_request.post_new_client_kit(kit_body)
@@ -47,21 +22,55 @@ def negative_assert(kit_body):
     assert kit.json()["name"] == kit_body["name"]
 
 
-def test3():
+# Comprueba el número mínimo de caracteres permitidos: 1
+def test_min_character():
+    kit_body = data.kit_body.kit_body1
+    positive_assert(kit_body)
+
+
+# Comprueba el número máximo de caracteres permitidos: 511
+def test_max_character():
+    kit_body = data.kit_body.kit_body2
+    positive_assert(kit_body)
+
+
+# Comprueba el número por debajo de los caracteres permitidos: 0
+def test_zero_character():
     kit_body = data.kit_body.kit_body3
-    positive_assert(kit_body)
+    negative_assert(kit_body)
 
 
-def test4():
+# Comprueba el número por encima de los caracteres permitidos: 512
+def test_over_character():
     kit_body = data.kit_body.kit_body4
+    negative_assert(kit_body)
+
+
+# Comprueba los caracteres especiales
+def test_special_character():
+    kit_body = data.kit_body.kit_body5
     positive_assert(kit_body)
 
 
-def test8():
+# Comprueba los espacios entre caracteres
+def test_space_between_character():
+    kit_body = data.kit_body.kit_body6
+    positive_assert(kit_body)
+
+
+# Comprueba los números entre caracteres
+def test_numbers():
+    kit_body = data.kit_body.kit_body7
+    positive_assert(kit_body)
+
+
+# Comprueba cuando el parámetro no se pasa en la solicitud
+def test_no_parameters():
     kit_body = data.kit_body.kit_body8
-    positive_assert(kit_body)
+    negative_assert(kit_body)
 
 
-def test9():
+# Comprueba cuando se pasa un tipo diferente de parámetro en la solicitud
+def test_different_kind_of_character():
     kit_body = data.kit_body.kit_body9
-    positive_assert(kit_body)
+    negative_assert(kit_body)
